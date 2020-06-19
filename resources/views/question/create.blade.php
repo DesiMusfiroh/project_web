813f94b0ff830ae34b86b5871c0f36dafd7c3806
krismandev <krismanpratama@gmail.com>
2020-06-20 02:31:42 +0700

    coba mengedit

--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -6,9 +6,9 @@
         <div class="card-header  pt-3 pb-2 text-center" style="border-radius: 20px 20px 0px 0px; background-color:#7BEDC4;">
             <h4 class="card-title"> Buat paket soal  </h4>
         </div>
-        <div class="card-body">             
-            <div class="container">                                                                  
-                                    
+        <div class="card-body">
+            <div class="container">
+
                 <input type="hidden" name="user_id" value="6 ">
                 <div class="form-row mb-0 mt-0 pt-0">
                     <div class="form-group col-md-6">
@@ -20,8 +20,11 @@
                         <input type="text" class="form-control" id="durasi" name="durasi" style="border-radius:10px; box-shadow: 3px 0px 5px grey;">
                     </div>
                 </div>
-                <div class="text-right"> <button type="submit" class="btn btn-primary" style="box-shadow: 3px 2px 5px grey;">Save </button> </div> 
-            </div>           
+                <div class="">
+                  djsdhshd
+                </div>
+                <div class="text-right"> <button type="submit" class="btn btn-primary" style="box-shadow: 3px 2px 5px grey;">Save </button> </div>
+            </div>
         </div>
     </div>
 </div>
