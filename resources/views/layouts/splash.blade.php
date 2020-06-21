d10e47fcc0c7ce25dbb6c896e8e5dddf90cab4ee
Repaldi <repaldi25@gmail.com>
2020-06-21 13:10:11 +0700

    Perubahan Login

--- resources/views/layouts/splash.blade.php
+++ resources/views/layouts/splash.blade.php
@@ -17,6 +17,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Bowlby+One+SC&family=Bungee&family=Chelsea+Market&family=Lilita+One&family=Pangolin&family=Rambla&family=Tenali+Ramakrishna&display=swap');
