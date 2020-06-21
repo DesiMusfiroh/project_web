a1e974c6b77bf52c1d8676fdb31c14c9680027df
Repaldi <repaldi25@gmail.com>
2020-06-21 15:17:38 +0700

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,39 @@
 @extends('layouts.splash')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" >
-                <div class="card-header">{{ __('Login') }}</div>
+    <div  class="col-md-5">
+     <div id="card"  style="border-radius:20px;  box-shadow: 10px 10px 10px grey;
+        background: linear-gradient(180deg, rgba(17, 0, 23, 0.96) 0%, rgba(44, 5, 60, 0.96) 30.21%, #5E2575 70.83%, #BEA2CF 100%);  /* background sidebar */
+        -webkit-transition: all 0.3s; -o-transition: all 0.3s; transition: all 0.3s; ">
+                        <div class="card-header pt-3 pb-2 text-center" style="border-radius: 20px 20px 0px 0px; background-color : #EDE5E5;">
+                            <h4 class="card-title">Login</h4>
+                        </div>
+                        <div class="card-body"  >
+                                @if (session('success'))
+                                    <div class="alert alert-success">{{ session('success') }}</div>
+                                @endif
 
+                                @if (session('error'))
+                                    <div class="alert alert-danger">{{ session('error') }}</div>
+                                @endif
+    
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                        <label class="sr-only" for="email">{{ __('E-Mail Address') }}</label>
+                            <div class="col-md-10 offset-md-1">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text"><span class="fa fa-envelope"></span></div>
+                                 </div>
+                                         <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                             </div>   
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -26,11 +43,15 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                        <label class="sr-only" for="password">{{ __('Password') }}</label>
+                            <div class="col-md-10 offset-md-1">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text"><span class="fa fa-lock" ></span></div>
+                                 </div>
+                                         <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password"  required autocomplete="current-password">
+                             </div>   
+                            
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -40,7 +61,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-10 offset-md-1">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -52,8 +73,8 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-10 offset-md-1">
+                                <button type="submit">
                                     {{ __('Login') }}
                                 </button>
 
--- resources/views/layouts/splash.blade.php
+++ resources/views/layouts/splash.blade.php
@@ -15,8 +15,12 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Bowlby+One+SC&family=Bungee&family=Chelsea+Market&family=Lilita+One&family=Pangolin&family=Rambla&family=Tenali+Ramakrishna&display=swap');
