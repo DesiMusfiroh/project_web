fcd9b94600284ae63bff12c4202a5a5bc5e96233
Repaldi <repaldi25@gmail.com>
2020-07-14 22:40:09 +0700

    Merapihkan Download Hasil Ujian, Tampilan Soal di Pagination

--- resources/views/exams/pagination_data.blade.php
+++ resources/views/exams/pagination_data.blade.php
@@ -1,20 +1,35 @@
 <div class="row">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header" style=" background: #EDE5E5;">Soal Ujian</div>
-          <div class="card-body " >
-            <?php $i=0; ?>
+          <div class="card-header" style=" background: #EDE5E5;" >Soal Ujian</div>
+          <div class="card-body "  >
+         
+  <style type="text/css">
+
+		.pagination li{
+     
+			margin:5px;
+      
+		}
+    .li:after{
+
+    }
+   
+	</style>
+          
             @foreach($soal_satuan as $item)
-                <div class=" container row">
-                    <div class="col-md-3"><h6>Soal No.  <?php  $i++;  echo $i; ?> </h6></div>
+                
+                <div class=" container row" >
+                    <div class="col-md-3"><h6>Soal No.    </h6></div>
                     <div class="col-md-8 text-right"><h6>Poin : {{$item->poin}}</h6></div>
                 </div>
-                <div class="container">
+                <div class="container" >
                 <table >
                 @if($item->jenis == "Essay")
                     <b> Pertanyaan </b> :
-                        {{$item->essay->pertanyaan}}
+                        {!!$item->essay->pertanyaan!!}
                     <div class="mt-2">
+                    <b> Jawaban : </b> 
                       <textarea class="form-control" name="jawab" id="jawaban_essay" cols="30" rows="3" ></textarea>
                       <input type="hidden" id="essay_id" value="{{$item->essay->id}}">
                       <input type="hidden" id="user_id" value="{{Auth::user()->id}}">
@@ -22,7 +37,7 @@
 
                 @elseif($item->jenis == "Pilihan Ganda")
                     <tr>
-                        <td><p>{{$item->pilgan->pertanyaan}} </p> </td>
+                        <td><b> Pertanyaan </b> :<br/> <p>{!!$item->pilgan->pertanyaan!!} </p> </td>
                     </tr>
                     <tr>  
                         <td>
@@ -44,8 +59,8 @@
                 <hr>
             @endforeach
 
-            {!! $soal_satuan->links() !!}
-
+            
+           
           </div>
         </div>
       </div>
@@ -55,8 +70,8 @@
           <div class="card-header"  style=" background: #EDE5E5;">Navigasi</div>
           <div class="card-body">
             <div class="row ">
-              <div class="col-12 text-center ">
-
+              <div class="col-12 text-center " style=" overflow: Auto;">
+              {!! $soal_satuan->links() !!}
               </div>
             </div>
           </div>
